76e42724fe83365f41f68f992024e6400402b84b
kreuzer <t.kreuzer@fz-juelich.de>
2020-01-28 10:46:37 +0100

    improve logging for splunk

--- app/available_utils.py
+++ app/available_utils.py
@@ -8,12 +8,12 @@ from subprocess import STDOUT, check_output, CalledProcessError
 
 def available(app_logger, uuidcode, node):
     cmd_check = ['ssh', 'available_{}'.format(node)]
-    app_logger.trace("{} - CheckOutput: {}".format(uuidcode, cmd_check))
+    app_logger.trace("uuidcode={} - CheckOutput: {}".format(uuidcode, cmd_check))
     try:
         output = check_output(cmd_check, stderr=STDOUT, timeout=5)
     except CalledProcessError:
         return False
-    app_logger.trace("{} - Result: {}".format(uuidcode, output))
+    app_logger.trace("uuidcode={} - Result: {}".format(uuidcode, output))
     out = output.decode("utf-8")
-    app_logger.trace("{} - Return: {}".format(uuidcode, 'Jupyter@JSC is running' in out))
+    app_logger.trace("uuidcode={} - Return: {}".format(uuidcode, 'Jupyter@JSC is running' in out))
     return 'Jupyter@JSC is running' in out
